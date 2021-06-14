81a54bfb506832d46e542f2d08e416f53bf60e36
Ingrid <silva_ingrid@outlook.com>
2021-06-14 17:24:17 -0300

    acabei

---
+++ semana1/index.js
@@ -0,0 +1,28 @@
+// EXERCICIO//
+const nome1 = prompt ('Qual o seu nome ?')
+const idade1 = prompt ('Qual a sua idade?')
+console.log (nome1)
+console.log (idade1)
+console.log (typeof nome1, typeof idade1)
+console.log ('Olá', nome1,'você tem',idade1,'anos')
+
+// Imprimiu a tela /
+
+let cor = prompt ('está de preto hoje?')
+let almoço = prompt ('Já almoçou hoje?')
+let estudo = prompt ('Está estudando?')
+let resposta1 = 'SIM'
+let resposta2= 'NÃO'
+console.log (cor, " - ",resposta2)
+console.log (estudo, ' - ', resposta1)
+console.log (almoço, ' - ', resposta1) 
+
+let a = 10
+let b = 25
+let c 
+c = a
+a = b
+b = c
+console.log ('O novo valor de a é', a)
+console.log ('O novo valor de B é', b)
+
