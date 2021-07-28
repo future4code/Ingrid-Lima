930aa29b48973151f3b71ac9c1d841a52f89b6e4
Ingrid <silva_ingrid@outlook.com>
2021-07-28 16:56:17 -0300

    aula 26-07-2021

---
+++ semana07/labeusers/src/components/TelaListaUsuario.js
@@ -0,0 +1,32 @@
+import React from "react"
+import axios from "axios";
+
+
+ class TelaListaUsuario extends React.Component {
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    render() {
+        return (
+            <div>
+
+            <button onClick={this.props.irParaCadastro}>Ir para Cadastro</button>
+            <h2>Lista de Usuários</h2>
+
+
+        </div>
+        )
+    }
+
+}
+
+
+
+export default TelaListaUsuario;
