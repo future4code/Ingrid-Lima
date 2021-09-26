1308bb0758593ce3d5c8944fd4ccc39cc7cc1558
Ingrid <silva_ingrid@outlook.com>
2021-09-26 17:28:05 -0300

    código finaizado

--- semana15/Projeto/src/index.ts
+++ semana15/Projeto/src/index.ts
@@ -11,11 +11,47 @@ app.listen(3003, () => {
 
 })
 
-app.post("/users", (req: Request, res: Response) =>{
+app.post("/users/create", (req: Request, res: Response) =>{
 
     try {
+     const { name, cpf, dateOfBirthAsString} = req.body
+     const [day, month, year] = dateOfBirthAsString.split("/")
+     const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
+     const age : number = Date.now() - dateOfBirth.getTime()
+     const ageInYears: number = age / 1000 / 60 / 60 / 24 / 365
 
+     if(ageInYears < 18){
+         res.statusCode = 406 
+         throw new Error("Idade do usuário deve ser maior que 18 anos!!")
+     }
+
+     
+     accounts.push({
+         name,
+         cpf,
+         dateOfBirth,
+         balance:0,
+         statement: []
+     })
+     res.status(201).send("Conta Criada com Sucesso!!")
     } catch (error){
+        console.log(error)
+res.status(400).send(error)
+    }
+})
+
+app.get ("users/all", (req:Request, res: Response) => {
+    try{
+        if (!accounts.length){
+            res.statusCode = 404
+            throw new Error("Nenhuma conta foi encontrada!!")
+            
+        }
+        res.status(200).send(accounts)
+        
+
+    }catch (error) {
+        res.send(error)
 
     }
 })
