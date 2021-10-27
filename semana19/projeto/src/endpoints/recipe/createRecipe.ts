cedc83e681666295142f6abaa8a2a1556ecc6ca9
Ingrid <silva_ingrid@outlook.com>
2021-10-27 17:30:37 -0300

    Projeto finalizado

---
+++ semana19/projeto/src/endpoints/recipe/createRecipe.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import { connection } from "../../connection";
+import { getTokenData } from "../../services/authenticator";
+import generateId from "../../services/idGenerator";
+import { recipeTableName } from "../../types";
+
+export default async function createRecipe (
+    req: Request,
+    res: Response
+): Promise<void>{
+    try{
+
+        const token = req.headers.authorization
+        const {title, description} = req.body
+
+        const tokenData = getTokenData(token!)
+
+        if(!tokenData){
+            res.statusCode =401
+            throw new Error ("Recipe not found")
+        }
+
+        if(!title || !description){
+            res.statusCode =422
+            throw new Error ("'title' and 'description' required")
+        }
+
+
+        const id = generateId()
+
+        const createdAt = new Date()
+
+        await connection(recipeTableName)
+        .insert({
+            id,
+            title,
+            description,
+            created_at: createdAt,
+            author_id: tokenData
+        })
+
+
+        res.send("Recipe successfuly created!")
+
+    } catch (error) {
+        console.log(error);
+        
+        if (res.statusCode === 200){
+            res.status (500).send("Internal server error")
+        } else {
+            res.send(error)
+        }
+        
+      }
+    }
